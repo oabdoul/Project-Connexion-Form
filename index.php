aee385ad5e9fc76f055bf4dbb1d9102a30eedc36
Abdoul <abdoulouedraogo95@gmail.com>
2025-01-14 22:32:17 +0100

    Modification du fichier index.html  page inscription

--- index.php
+++ index.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Formulaire de connexion</title>
+    <title>Formulaire d'inscription</title>
 </head>
 <body>
     <form action="submit-login.php" method="POST">
-        <h1>Connexion</h1>
+        <h1>Inscription</h1>
         <div>
             <label for="username">Nom et Prenom</label>
             <input type="text" name="username" id="username" placeholder="John Doe">
         </div>
         <div>
             <label for="email">Adresse Mail</label>
-            <input type="email" name="email" id="email" placeholder="abdoul@gmail.com">
+            <input type="email" name="email" id="email" placeholder="john.doe@gmail.com">
         </div>
         <div>
-            <label for="password">Mots de passe</label>
-            <input type="password" name="password" id="password">
+            <label for="mdp">Mots de passe</label>
+            <input type="password" name="mdp" id="mdp">
         </div>
-        <button type="submit">Valider</button>
+        <button type="submit" name="ok">Valider</button>
+        <p>Vous avez déjà un compte ? <a href="connexion.php">Se connecter</a></p>
     </form>
 </body>
 </html>
